826710bd357056a01307031332abd9792e30960e
Thottempudi, Harsha <kthottempudi3@gatech.edu>
2023-09-26 22:00:31 -0400

    Initialized dashboard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./Pages/AuthPage";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      <Route path="/dashboard" element={<Dashboard />} />
       {/* {/* <Route path="/login" element={<LoginPage />} /> */}
     </Routes>
   );
