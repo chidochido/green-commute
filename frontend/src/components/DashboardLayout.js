15349ddf399a338c45ddc188ba5d7d55c7ab20d4
Thottempudi, Harsha <kthottempudi3@gatech.edu>
2023-10-02 21:09:14 -0400

    Added map with directions rendering

--- frontend/src/components/DashboardLayout.js
+++ frontend/src/components/DashboardLayout.js
@@ -1,16 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as React from "react";
 
 import { Stack, Box, Divider } from "@mui/material";
-import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  DirectionsRenderer,
+  Marker,
+} from "@react-google-maps/api";
 
 function DashboardLayout(props) {
+  const center = { lat: 33.772378580982, lng: -84.39474039330827 }; // Tech Tower
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GMAPS_JS_API_KEY,
   });
 
   if (!isLoaded) {
+    return "LOADING";
   }
 
   return (
@@ -21,14 +32,25 @@ function DashboardLayout(props) {
         <Divider orientation="vertical" flexItem sx={{ width: "0.1vw" }} />
       }
     >
-      {!isLoaded && (
-        <Box sx={{ bgcolor: "red", height: "100vh", width: "100vw" }}></Box>
-      )}
-      {isLoaded && (
-        <Box sx={{ bgcolor: "grey", height: "100vh", width: "100vw" }}>
-          <GoogleMap></GoogleMap>
-        </Box>
-      )}
+      <Box sx={{ bgcolor: "grey", height: "100vh", width: "100vw" }}>
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          options={{
+            zoomControl: true,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={(map) => setMap(map)}
+        >
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </Box>
 
       <Box sx={{ bgcolor: "blue", height: "100vh", width: "100vw" }} />
     </Stack>
